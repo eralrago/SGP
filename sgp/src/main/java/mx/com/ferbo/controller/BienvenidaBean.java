03120abf770a13884c42e1ecaa5d2d249e178643
Ernesto <eralrago@gmail.com>
2023-02-01 13:32:37 -0600

    despligue de mensaje de cumpleaños

--- sgp/src/main/java/mx/com/ferbo/controller/BienvenidaBean.java
+++ sgp/src/main/java/mx/com/ferbo/controller/BienvenidaBean.java
@@ -2,12 +2,17 @@ package mx.com.ferbo.controller;
 
 
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
-import javax.faces.context.FacesContext;
+//import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletRequest;
 
 import mx.com.ferbo.dao.EmpleadoDAO;
 import mx.com.ferbo.dto.DetEmpleadoDTO;
@@ -19,6 +24,13 @@ public class BienvenidaBean implements Serializable {
 	private DetEmpleadoDTO empleadoSelected;
 	private final EmpleadoDAO empleadoDAO;
 	private String numeroEmpl;
+	private String strCumpleanios;
+	private DateFormat dateFormat;
+	private Date fechaActual;
+	private LocalDate currentDate;
+	private LocalDate fechaCumpleanios;
+	private String strFechaActual;
+	private String strFechaCumpleanios;
 	
 	/*private FacesContext faceContext;
     private HttpServletRequest httpServletRequest;*/
@@ -27,14 +39,15 @@ public class BienvenidaBean implements Serializable {
 		empleadoDAO = new EmpleadoDAO();
 		empleadoSelected = new DetEmpleadoDTO();
 		setNumeroEmpl("0006");
+		dateFormat = new SimpleDateFormat("yyyy-MM-dd");  
+		fechaActual = new Date();
+		strFechaActual = getDateFormat().format(getFechaActual());
+		currentDate = LocalDate.parse(strFechaActual);
+		strCumpleanios = "";
 	}
 	
 	@PostConstruct
     public void init() {
-		/*faceContext = FacesContext.getCurrentInstance();
-        httpServletRequest = (HttpServletRequest) faceContext.getExternalContext().getRequest();
-        this.empleadoSelected = (DetEmpleado) httpServletRequest.getSession(true).getAttribute("nombre");*/
-		// System.out.println(empleadoSelected.getNumEmpleado());
 		empleadoLogeado();
     }
 
@@ -44,9 +57,18 @@ public class BienvenidaBean implements Serializable {
 	}
 	
 	public String empleadoLogeado() {
-		// DetEmpleadoDTO empleadoSeleccionado = new DetEmpleadoDTO();
-		// String numeroEmpl = getEmpleadoDAO();
 		empleadoSelected = empleadoDAO.buscarPorNumEmpl(getNumeroEmpl());
+		int diaActual = currentDate.getDayOfMonth();
+		int mesActual = currentDate.getMonthValue();
+		strFechaCumpleanios = getDateFormat().format(empleadoSelected.getFechaNacimiento());
+		fechaCumpleanios = LocalDate.parse(strFechaCumpleanios);
+		int diaCumpleanios = fechaCumpleanios.getDayOfMonth();
+		int mesCumpleanios = fechaCumpleanios.getMonthValue();
+		System.out.println(diaActual + " " + mesActual + " // " + diaCumpleanios + " " + mesCumpleanios);
+		if(diaActual == diaCumpleanios && mesActual == mesCumpleanios) {
+			//setStrCumpleanios("¡Feliz Cumpleaños " + empleadoSelected.getNombre() + " " + empleadoSelected.getPrimerAp() + " " + empleadoSelected.getSegundoAp() + "!");
+			strCumpleanios = "¡Feliz Cumpleaños " + empleadoSelected.getNombre() + " " + empleadoSelected.getPrimerAp() + " " + empleadoSelected.getSegundoAp() + "!";
+		} 
 		return "protected/registroAsistencia.xhtml";
 	}
 
@@ -69,6 +91,48 @@ public class BienvenidaBean implements Serializable {
 	public void setNumeroEmpl(String numeroEmpl) {
 		this.numeroEmpl = numeroEmpl;
 	}
+
+	public String getStrCumpleanios() {
+		return strCumpleanios;
+	}
+
+	public void setStrCumpleanios(String strCumpleanios) {
+		this.strCumpleanios = strCumpleanios;
+	}
+
+	public DateFormat getDateFormat() {
+		return dateFormat;
+	}
+
+	public void setDateFormat(DateFormat dateFormat) {
+		this.dateFormat = dateFormat;
+	}
+
+	public Date getFechaActual() {
+		return fechaActual;
+	}
+
+	public void setFechaActual(Date fechaActual) {
+		this.fechaActual = fechaActual;
+	}
+
+	public LocalDate getCurrentDate() {
+		return currentDate;
+	}
+
+	public void setCurrentDate(LocalDate currentDate) {
+		this.currentDate = currentDate;
+	}
+
+	public String getStrFechaActual() {
+		return strFechaActual;
+	}
+
+	public void setStrFechaActual(String strFechaActual) {
+		this.strFechaActual = strFechaActual;
+	}
+
+	
 	
 	
 }

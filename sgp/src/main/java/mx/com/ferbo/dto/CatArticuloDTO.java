702e2547e002791967c6936a145e67aa6fe986f9
Ernesto <eralrago@gmail.com>
2023-02-23 10:28:46 -0600

    Desarrollando

---
+++ sgp/src/main/java/mx/com/ferbo/dto/CatArticuloDTO.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.com.ferbo.dto;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+public class CatArticuloDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer idArticulo;
+    private String descripcion;
+    private short activo;
+
+    public CatArticuloDTO() {
+    }
+
+    public CatArticuloDTO(Integer idArticulo, String descripcion, short activo) {
+		super();
+		this.idArticulo = idArticulo;
+		this.descripcion = descripcion;
+		this.activo = activo;
+	}
+
+    public Integer getIdArticulo() {
+        return idArticulo;
+    }
+
+    public void setIdArticulo(Integer idArticulo) {
+        this.idArticulo = idArticulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public short getActivo() {
+        return activo;
+    }
+
+    public void setActivo(short activo) {
+        this.activo = activo;
+    }
+
+}
